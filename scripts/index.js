c3fcbd2093abe982749df5276cd2b12f93159d3c
llsdog <1438212369@qq.com>
2025-07-02 08:32:17 +0800

    添加按钮热切换

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
+function switchButtonToReadMe () {
+    console.log("切换分页");
+    
+    const bodyHomeTop = document.querySelector("#body-home-top");
+    
+    bodyHomeTop.style.backgroundImage = "url(./src/img/LL_sun.jpg)";
+}
 
-
+function switchButtonToMainPage () {
+    console.log("切换主页");
+    
+    const bodyHomeTop = document.querySelector("#body-home-top");
+    
+    bodyHomeTop.style.backgroundImage = "url(./src/img/LL_moon.jpg)";
+}
 
